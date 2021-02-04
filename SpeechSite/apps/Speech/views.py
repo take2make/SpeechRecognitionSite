8e66a6c4f7eeaa291ae59d982fa2d3e99c63b93a
Alexey Lyutikh <lutixalex1998@gmail.com>
2021-02-04 19:11:15 +0300

    new

--- SpeechSite/apps/Speech/views.py
+++ SpeechSite/apps/Speech/views.py
@@ -2,31 +2,29 @@ from django.shortcuts import render, redirect
 from django.core.files.storage import FileSystemStorage as File
 from .forms import SelectForm
 from .forms import language_model_choices
-from Speech2Text import combine
+import scripts
 import os
-from pathlib import Path
 import threading
 import time
 
 
-def speech_to_txt(audio_file, model_choice):
-    model_choice = model_choice.split(' ')
+api_url = "http://192.168.1.218:8000/api/speech_api/"
 
-    model = os.path.join('Speech2Text', 'model_'+model_choice[0]+'_'+model_choice[1])
-    audio = os.path.join('media', audio_file)
 
-    #-------------start recognition and combining-----------------#
-    print(f'\n your model {model}\n and audio_file {audio} \n')
-    combine.main(audio, model)
-    #-------------------------------------------------------------#
-    pass
-
+def get_data_from_session(url_session):
+    while True:
+        time.sleep(1)
+        response = scripts.get_json(url_session)
+        print(response)
+        if response['detail'] == 200:
+            result_txt = response['result']
+            break
 
 
 def index(request):
     form = SelectForm()
 
-    context = {'form': form,'text':'result', 'loading': False}
+    context = {'form': form,'text':'result'}
 
     if (request.method == 'POST'):
 
@@ -39,14 +37,21 @@ def index(request):
         fs.save(uploaded_file.name, uploaded_file)
 
         print('uploaded_file is \n',uploaded_file.name, '\n and model is', model)
-        main_thread = threading.Thread(target = speech_to_txt, args=(uploaded_file.name, model,))
-        main_thread.start()
-        main_thread.join()
 
-        result = os.path.join('txt', 'out.txt')
-        with open(result, 'r') as file:
-            result_txt = file.read()
+        file = os.path.join('media', uploaded_file.name)
+        encoded_data = scripts.encode_file(file)
+        os.remove(file)
+        extension = uploaded_file.name.split('.')[-1]
 
-        context = {'form': form, 'text': result_txt, 'loading': False}
+        response = scripts.send_json(api_url, encoded_data, extension, model)
+        if response['detail'] == 200:
+            session_id = response['session_id']
+            url_session = f'{api_url}{session_id}/'
+            main_thread = threading.Thread(target=get_data_from_session, args=(url_session,))
+            main_thread.start()
+            main_thread.join()
+
+            result_txt = scripts.get_json(url_session)['result']
+            context = {'form': form, 'text': result_txt}
 
     return render(request, 'index.html', context=context)
---
+++ scripts.py
@@ -0,0 +1,19 @@
+import requests
+import base64
+
+
+def get_json(url):
+    response = requests.get(url)
+    return response.json()
+
+
+def encode_file(file):
+	with open(file, 'rb') as file:
+		encoded_data = base64.b64encode(file.read())
+	return encoded_data
+
+
+def send_json(url, encoded_data, extension, model):
+    params = {'encoded_data': encoded_data, 'ext': extension, 'model': model}
+    response = requests.post(url, data=params)
+    return response.json()
